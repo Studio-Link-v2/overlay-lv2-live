885309862853798dd2d43191fd469190af3e5f8c
Sebastian Reimers <info@miete-admin.de>
2016-12-04 16:49:12 +0100

    fix paths

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-gcc -g -Wall -shared -o studio-link-live.so -fPIC studio-link.c ../baresip/libbaresip_live.a \
+gcc -g -Wall -shared -o studio-link-live.so -fPIC studio-link-live.c ../my_include/libbaresip_live.a \
     ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include \
     ../openssl/libssl.a ../openssl/libcrypto.a -lm -lpthread -lz -ldl -lresolv
 
