d58f3035208672f3a6f3e21bb6d0232c06cbab2d
Sebastian Reimers <hallo@studio-link.de>
2020-12-24 10:07:37 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 gcc -g -Wall -shared -o studio-link-onair.so -fPIC studio-link-onair.c ../my_include/libbaresip_onair.a \
-    ../re/libre.a ../rem/librem.a ../opus/libopus.a -I../my_include \
+    ../re/libre.a ../rem/librem.a ../3rdparty/lib/libopus.a -I../my_include \
      -lm -lpthread -lz -ldl -lresolv -lcrypt -lssl
 
 ldd studio-link-onair.so
